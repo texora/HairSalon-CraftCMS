2e459533bfe805745412d0aca4917eb10f0786b8
Bhashkar Yadav <bhashkar007@gmail.com>
2024-08-27 17:49:13 +0530

    Holding page

---
+++ web/static/js/custom.js
@@ -0,0 +1,29 @@
+$("document").ready(function () {
+    $('.nav-icon').click(function(){
+          $(this).toggleClass('open');
+           $('.header').toggleClass('open-header');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+     const controls = [
+        'play-large', // The large play button in the center
+        'restart', // Restart playback
+        'rewind', // Rewind by the seek time (default 10 seconds)
+        'play', // Play/pause playback
+        //'fast-forward', // Fast forward by the seek time (default 10 seconds)
+        'progress', // The progress bar and scrubber for playback and buffering
+        //'current-time', // The current time of playback
+        'duration', // The full duration of the media
+        'mute', // Toggle mute
+        'volume', // Volume control
+        //'captions', // Toggle captions
+        //'settings', // Settings menu
+        //'pip', // Picture-in-picture (currently Safari only)
+        //'airplay', // Airplay (currently Safari only)
+        'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+        'fullscreen' // Toggle fullscreen
+    ];
+    const player = Plyr.setup('.video-player', { controls });
+});
+
